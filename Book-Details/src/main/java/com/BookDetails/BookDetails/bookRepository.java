428951384aeeac351f999c47b0db0d245e98f709
Bluepelota <80494809+Bluepelota@users.noreply.github.com>
2023-11-06 15:59:07 -0500

    re-uploading new file for book details

--- Book-Details/src/main/java/com/BookDetails/BookDetails/bookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BookDetails.BookDetails;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface bookRepository extends JpaRepository<createBook, String> {
-    
-    
-}
-
