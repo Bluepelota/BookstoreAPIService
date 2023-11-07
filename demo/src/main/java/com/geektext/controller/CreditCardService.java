1d05a4da445ef8b4b9fbdca0273e3b44d419fffe
soniprrzz <145025612+soniprrzz@users.noreply.github.com>
2023-11-06 19:30:46 -0500

    Delete demo directory

--- demo/src/main/java/com/geektext/controller/CreditCardService.java
+++
@@ -1,9 +0,0 @@
-package com.geektext.controller;
-
-public class CreditCardService {
-
-    public boolean createCreditCard(String username, CreditCard creditCard) {
-        return false;
-    }
-
-}
