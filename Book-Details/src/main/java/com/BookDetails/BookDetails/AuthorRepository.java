a0e5da3ad9139bb97feb8a09820b4d7308c7d3aa
Bluepelota <80494809+Bluepelota@users.noreply.github.com>
2023-11-27 12:39:49 -0500

    Uploading new book-details before i update it again

--- Book-Details/src/main/java/com/BookDetails/BookDetails/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.BookDetails.BookDetails;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<byAuthor, Long> {
-    
-}
--- Book-Details/src/main/java/com/BookDetails/BookDetails/BookDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.BookDetails.BookDetails;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookDetailRepository extends JpaRepository<bookDetails, String> {
-
-}
